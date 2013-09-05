208170c6ab1765b2d65b4febd10ed9f447a8ac3d
Miguel Ángel Sánchez Chordi <slayer.ma@gmail.com>
2013-09-05 21:08:07 +0200

    Register Nickname and User list
    Implemented the nickname registering and the userlist broadcast emision
    to set up-to-date all the clients

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var _ = require('underscore');
 
 var app = express();
 
@@ -40,23 +41,44 @@ server.listen(app.get('port'), function(){
 
 //Bindings de sockets
 var totalUsers = 0;
+var nicknames = [];
 io.sockets.on('connection', function(socket){
 	totalUsers++;
 	socket.emit('totalUsers', {'totalUsers': totalUsers});
 	socket.broadcast.emit('totalUsers', {'totalUsers': totalUsers});
+    socket.broadcast.emit("userList",{'userList': nicknames});
 	console.log("Otro pollo en el corral");
 
 	socket.on('disconnect', function(){
 		totalUsers--;
 		socket.broadcast.emit('totalUsers', {'totalUsers': totalUsers});
 		console.log("Un pollo abandona en el corral");
-	})
+        socket.broadcast.emit("userList",{'userList': nicknames});
+	});
+
+    socket.on("set-nickname", function(data){
+        var exist = _.find(nicknames, function(nick){ return nick == data.nick; });
+        if(exist){
+            socket.emit("nick-exists", { 'msg': 'Nick already exists'});
+        }else{
+            nicknames.push(data.nick);
+            socket.set('nick', data.nick, function(){
+                socket.emit("nick-saved",{'nick': data.nick});
+                socket.broadcast.emit("userList",{'userList': nicknames});  
+            });
+            
+        }
+    });
 
     socket.on('message-sent', function(data){
         var d = new Date();
         var formatTime =  d.toLocaleTimeString();
         //socket.emit('message-broadcast', {'message': data.message, 'time': formatTime });
-        socket.broadcast.emit('message-broadcast', {'message': data.message, 'time': formatTime });
+        socket.get('nick',function(error, nick){
+            console.log("Error: " + error);
+            socket.broadcast.emit('message-broadcast', {'message': data.message, 'nick': nick, 'time': formatTime });
+        })
+        
     });
 
 });
